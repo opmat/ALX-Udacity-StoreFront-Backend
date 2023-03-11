631706848ec894905a789ba5852d76d56c3ec265
opmat <opmat01@yahoo.com>
2023-03-11 23:05:14 +0100

    chore: postgres init script update

--- dumps/init.db.sh
+++ dumps/init.db.sh
@@ -6,4 +6,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
     DROP DATABASE storefront_test IF EXISTS;
     CREATE DATABASE storefront_test;
     GRANT ALL PRIVILEGES ON DATABASE storefront_test TO alxtest;
+    COMMENT ON DATABASE storefront_dev
+        IS 'the main storefront database';
+    COMMENT ON DATABASE storefront_test
+        IS 'the storefront test database';
 EOSQL
---
+++ dumps/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE storefronts_dev;
+GRANT ALL PRIVILEGES ON DATABASE storefronts_dev TO alxtest;
+CREATE DATABASE storefronts_test;
+GRANT ALL PRIVILEGES ON DATABASE storefronts_test TO alxtest;
