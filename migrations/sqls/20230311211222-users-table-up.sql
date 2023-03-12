e4f7858bf3211dbf848943ede9b07531e15da66d
opmat <opmat01@yahoo.com>
2023-03-12 01:06:01 +0100

    chore: edit users table structure
    add unique constraint to email field

--- migrations/sqls/20230311211222-users-table-up.sql
+++ migrations/sqls/20230311211222-users-table-up.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     firstname VARCHAR(64) NOT NULL,
     lastname VARCHAR(64) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
     password_digest VARCHAR NOT NULL,
     date_registered  TIMESTAMPTZ NOT NULL
         DEFAULT (current_timestamp AT TIME ZONE 'UTC')
