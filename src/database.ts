f0662f38ee39d115c65ec4b40cef24c5b6eccb15
opmat <opmat01@yahoo.com>
2023-03-13 09:05:16 +0100

    chore: config update

--- src/database.ts
+++ src/database.ts
@@ -1,41 +1,41 @@
-import dotenv from 'dotenv'
-import { Pool } from 'pg'
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
 
-dotenv.config()
+dotenv.config();
 
 const {
-    ENV,
-    POSTGRES_HOST,
-    POSTGRES_PORT,
-    POSTGRES_USER,
-    POSTGRES_PASSWORD,
-    POSTGRES_DB,
-    POSTGRES_DB_TEST,
-    ...OTHERS
-} = process.env
+  ENV,
+  POSTGRES_HOST,
+  POSTGRES_PORT,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_DB,
+  POSTGRES_DB_TEST,
+  ...OTHERS
+} = process.env;
 
-let client
-console.log(`ENV: ${ENV}`)
-console.log(`OTHER ENVs: ${JSON.stringify(OTHERS)}`)
+// let client: Pool;
+console.log(`ENV: ${ENV}`);
+console.log(`OTHER ENVs: ${JSON.stringify(OTHERS)}`);
 
-if(ENV === 'test') {
-  client = new Pool({
-    host: POSTGRES_HOST,
-    database: POSTGRES_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-    port: (POSTGRES_PORT as any) as number,
-  })
-}
+const db: string = (ENV === 'test' ? POSTGRES_DB_TEST : POSTGRES_DB) as string;
 
-if(ENV === 'dev') {
-  client = new Pool({
-    host: POSTGRES_HOST,
-    database: POSTGRES_DB_TEST,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-    port: (POSTGRES_PORT as any) as number,
-  })
-}
+const Client = new Pool({
+  host: POSTGRES_HOST,
+  database: db,
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  port: Number(POSTGRES_PORT)
+});
 
-export default client
+// if(ENV === 'dev') {
+//   client = new Pool({
+//     host: POSTGRES_HOST,
+//     database: POSTGRES_DB_TEST,
+//     user: POSTGRES_USER,
+//     password: POSTGRES_PASSWORD,
+//     port: (POSTGRES_PORT as any) as number,
+//   })
+// }
+
+export default Client;
