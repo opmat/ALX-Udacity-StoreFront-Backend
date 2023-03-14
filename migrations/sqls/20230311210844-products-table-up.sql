fbf02c339abef3288a165a2aac19fbe9f6f596fe
opmat <opmat01@yahoo.com>
2023-03-14 04:04:25 +0100

    chore: update db migrate schema

--- migrations/sqls/20230311210844-products-table-up.sql
+++ migrations/sqls/20230311210844-products-table-up.sql
@@ -1,7 +1,7 @@
 /* Replace with your SQL commands */
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
-    product_name VARCHAR(64) NOT NULL,
+    product_name VARCHAR(100) UNIQUE NOT NULL,
     price numeric(10,2) NOT NULL,
-    category VARCHAR(64)
+    category VARCHAR(100)
 );
--- migrations/sqls/20230311211250-orders-table-down.sql
+++ migrations/sqls/20230311211250-orders-table-down.sql
@@ -1,2 +1,3 @@
 /* Replace with your SQL commands */
 DROP TABLE IF EXISTS orders;
+DROP TYPE IF EXISTS ostatus;
