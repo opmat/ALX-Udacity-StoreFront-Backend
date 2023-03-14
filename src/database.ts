e5a22ce9fdedbad1881514c42690afa643f10f6f
opmat <opmat01@yahoo.com>
2023-03-14 04:05:38 +0100

    chore: clean up

--- src/database.ts
+++ src/database.ts
@@ -10,13 +10,8 @@ const {
   POSTGRES_USER,
   POSTGRES_PASSWORD,
   POSTGRES_DB,
-  POSTGRES_DB_TEST,
-  ...OTHERS
+  POSTGRES_DB_TEST
 } = process.env;
-
-// let client: Pool;
-console.log(`ENV: ${ENV}`);
-console.log(`OTHER ENVs: ${JSON.stringify(OTHERS)}`);
 
 const db: string = (ENV === 'test' ? POSTGRES_DB_TEST : POSTGRES_DB) as string;
 
@@ -27,15 +22,5 @@ const Client = new Pool({
   password: POSTGRES_PASSWORD,
   port: Number(POSTGRES_PORT)
 });
-
-// if(ENV === 'dev') {
-//   client = new Pool({
-//     host: POSTGRES_HOST,
-//     database: POSTGRES_DB_TEST,
-//     user: POSTGRES_USER,
-//     password: POSTGRES_PASSWORD,
-//     port: (POSTGRES_PORT as any) as number,
-//   })
-// }
 
 export default Client;
