c4fd3044719472c6a23fa55acda02b6547ae7cbd
opmat <opmat01@yahoo.com>
2023-03-14 04:07:52 +0100

    feat: add cart

--- src/handlers/orders.route.ts
+++ src/handlers/orders.route.ts
@@ -1,11 +1,13 @@
 import { Request, Response, Router } from 'express';
 import { Order, OrderStore } from '../models/orders';
+import { DashboardService } from '../services/dashboard.service';
 import dotenv from 'dotenv';
 import verifyToken from '../middlewares/verifyToken';
 
 dotenv.config();
 
 const store = new OrderStore();
+const dashboardService = new DashboardService();
 const orderRouter = Router();
 
 const index = async (_req: Request, res: Response) => {
@@ -20,6 +22,27 @@ const index = async (_req: Request, res: Response) => {
 const show = async (req: Request, res: Response) => {
   try {
     const order = await store.show(Number(req.params.id));
+    res.json(order);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+const cart = async (req: Request, res: Response) => {
+  try {
+    const order = await store.show(Number(req.params.id));
+    const products = await dashboardService.orderProductList(
+      Number(req.params.id)
+    );
+    res.json([order, products]);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+const closeOrder = async (req: Request, res: Response) => {
+  try {
+    const order = await store.closeOrder(Number(req.body.id));
     res.json(order);
   } catch (error) {
     res.status(400).json(error);
@@ -44,8 +67,8 @@ const create = async (req: Request, res: Response) => {
 const addProductToOrder = async (req: Request, res: Response) => {
   try {
     const products = await store.addProductToOrder(
-      req.body.orderId as number,
-      req.body.productId as number,
+      req.body.order_id as number,
+      req.body.product_id as number,
       req.body.quantity as number
     );
     res.json(products);
@@ -82,10 +105,12 @@ const showCompletedOrder = async (req: Request, res: Response) => {
   }
 };
 
-orderRouter.post('/', verifyToken, create);
-orderRouter.post('/addproduct', verifyToken, addProductToOrder);
 orderRouter.get('/', index);
 orderRouter.get('/:id', show);
+orderRouter.get('/:id/cart', cart);
+orderRouter.post('/', verifyToken, create);
+orderRouter.post('/close', verifyToken, closeOrder);
+orderRouter.post('/addproduct', verifyToken, addProductToOrder);
 orderRouter.get('/user/:userid/active/:limit?', verifyToken, showCurrentOrder);
 orderRouter.get(
   '/user/:userid/complete/:limit?',
